d215b849651d5264acf1b7293d2b58b95d98ffc5
NikMel95 <nikmel95@mail.ru>
2016-12-12 01:20:17 +0300

    issue 2

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var OPERATORMAP = [
   {type: left, ops: parseOperatorsExp({Leq: '<='})},
   {type: left, ops: parseOperatorsExp({Greq: '>='})},
   {type: left, ops: parseOperatorsExp({Multi: '*'})},
-  {type: left, ops: parseOperatorsExp({Divide: '/'})},
+  {type: left, ops: parseOperatorsExp({Div: '/'})},
   {type: left, ops: parseOperatorsExp({Plus: '+'})},
   {type: left, ops: parseOperatorsExp({Minus: '-'})},
   {type: left, ops: parseOperatorsExp({And: '&&'})},
@@ -70,7 +70,7 @@ let WH = (obj) => {
   
   return `WH("${obj.value}",${obj.start.line}, ${obj.start.column}, ${obj.end.column})`
 }
-
+//.mark().map((obj) => Bracket(obj))
 let pIf = P.string('if').mark().map((obj) => keywordLex(obj))
 
 let pFi = P.string('fi').mark().map((obj) => keywordLex(obj))
@@ -89,6 +89,7 @@ let pLB = P.string("(").mark().map((obj) => Bracket(obj))
 let pRB = P.string(")").mark().map((obj) => Bracket(obj))
 
 let whitespace = P.regexp(/\s*/m).mark().map((obj) => WH(obj))
+//let whitespace = P.regexp(/\s*/m)
 let indentifier = P.regexp(/(?!read|write|skip|do|while|if|then|else|fi|od)[a-zA-Z_][a-zA-Z0-9_]*/).desc('identifier').mark().map((obj) => Var(obj))
 
 let VarOrNum = P.alt(indentifier, pNum)
@@ -123,9 +124,8 @@ function left(op, next) {
 
 let parseExp =
   P.lazy(() => {
-    return pLB
-      .then(parseJoinedExp)
-      .skip(pRB)
+
+    return P.seq(pLB,parseJoinedExp,pRB)
       .or(VarOrNum);
   });
 
@@ -183,9 +183,180 @@ let mainParse = P.lazy(() => {
 })
 
 let source = path.resolve(__dirname, process.argv[2])
-let result = mainParse.parse(fs.readFileSync(source, 'utf-8'))
+let result = mainParse.parse(fs.readFileSync(source, 'utf8'))
+let rs = util.inspect(result, {depth: null, colors: 'auto'});
+let rs1 = util.inspect(result,{depth:null});
+console.log(rs)
 
-console.log(util.inspect(result, {depth: null, colors: 'auto'}))
+
+const OPERATORMAP1 = {
+  'Plus':'+' ,
+  'Minus':'-' ,
+  'Multi':'*' ,
+  'Div':'/' ,
+  'Mod':'%',
+  'Eq':'==',
+  'Uneq':'!=' ,
+  'Gr':'>' ,
+  'Greq':'>=',
+  'Less':'<',
+  'Leq':'<=' ,
+  'And':'&&' ,
+  'Or':'||',
+  'Negative':'-'
+}
+
+
+
+
+var s = rs1.split("value:")[1];
+
+s = s.replace(/}/g,"")
+s = s.replace(/"/g,'\\"')
+s = s.replace(/'/g,"\"")
+
+
+
+let code = JSON.parse(s);
+var res = "";
+var arr_pp = [];
+function getCode(json_arr) {
+  
+  for(c in json_arr) {
+      if (json_arr[c] instanceof Object) {
+
+          getCode(json_arr[c]);
+          continue;
+      }
+     
+     var s = keyWord(json_arr[c]);
+     if(s!=''){
+        arr_pp.push(s)
+     }
+     
+
+  }
+
+}
+
+function keyWord(c){
+  if(c.search(/KW_Fi/i) == 0){
+    return 'fi';
+  }
+  else if(c.search(/KW_If/i) == 0){
+    return 'if';
+  }
+  else if(c.search(/KW_Do/i) == 0){
+    return 'do';
+  }
+  else if(c.search(/KW_Od/i) == 0){
+    return 'od';
+  }
+  else if(c.search(/KW_Then/i) == 0){
+    return 'then';
+  }
+  else if(c.search(/KW_Else/i) == 0){
+    return 'else';
+  }
+  else if(c.search(/KW_While/i) == 0){
+    return 'while';
+  }
+  else if(c.search(/KW_Read/i) == 0){
+    return 'read';
+  }
+  else if(c.search(/KW_Write/i) == 0){
+    return 'write';
+  }
+  else if(c.search(/KW_Skip/i) == 0){
+    return 'skip';
+  }
+  else if(c.search(/Colon/i) == 0){
+    return ';';
+  }
+  else if(c.search(/Assignment/i) == 0){
+    return ':=';
+  }
+  else if(c.search(/Var/i) == 0 || c.search(/Num/i) == 0){
+    var von = c.split("(")[1].split(",")[0]
+    von = von.replace(/"/g,'')
+    return von;
+  }
+  else if(c.search(/Bracket/i) == 0){
+    
+    var n = c.split('"');
+    
+    return n[1];
+  }
+  else if(c.search(/Op/i) == 0){
+    var von = c.split("(")[1].split(",")[0]
+    von = von.replace(/"/g,'')
+    return OPERATORMAP1[von];
+  }
+  else{
+    return '';
+  }
+  
+}
+
+function Ntab(c){
+  var s = "";
+  for(var i = 0;i<c;i++){
+    s += " ";
+  }
+  return s;
+}
+var one_check = false;
+function Pretty_Printer(arr){
+  var l = arr.length;
+  var level = 0;
+  for (var i = 0;i < l;i++){
+    
+    if(arr[i] == "then" || arr[i] == "do"){
+      res += arr[i] + " ";  
+      level++;
+      res += "\n" + Ntab(level*2);
+    }
+    else if(arr[i] == "fi" || arr[i] == "od"  || arr[i] == "else"){
+      res += "\n";
+      
+      level--;
+      res +=  Ntab(level*2) + arr[i] + " ";
+      if(arr[i] == "else"){
+        
+        level ++;
+        res += "\n" + Ntab(level*2);
+      }
+    }
+    else if(arr[i] == "if"){
+      res +=  arr[i] + " ";
+    }
+    else if(arr[i] == ";"){
+      res += arr[i] + "\n" + Ntab(level*2);
+
+    }
+    else if(arr[i] == "("){
+      res += arr[i]
+
+    }else{
+
+      
+      if(arr[i+1] == ";"){
+        res += arr[i];
+      } 
+      else if(arr[i+1] == ")"){
+        res += arr[i];
+      } else{
+        res +=  arr[i] + " ";
+      }       
+    } 
+  }
+}
+if(process.argv[3] == "-p"){
+  getCode(code);
+  Pretty_Printer(arr_pp);
+  console.log("\nPretty Printer:\n");
+  console.log(res);
+}
 
 
 
--- is2.js
+++ is2.js
@@ -3,20 +3,167 @@ var fs = require('fs')
 var path = require('path')
 const util = require('util')
 
+
 let source = path.resolve(__dirname, process.argv[2]);
-let result = fs.readFileSync(source, 'utf-8');
-let s = JSON.parse(result);
-var re = "";
+let s = fs.readFileSync(source, 'utf-8')
 
 
-function getCode(json_arr) {
-	for(c in json_arr) {
-	    if (json_arr[c] instanceof Object) {
-	        getCode(json_arr[c]);
-	        continue;
-	    }
-	   re += json_arr[c];
-	}
+const OPERATORMAP1 = {
+  'Plus':'+' ,
+  'Minus':'-' ,
+  'Multi':'*' ,
+  'Div':'/' ,
+  'Mod':'%',
+  'Eq':'==',
+  'Uneq':'!=' ,
+  'Gr':'>' ,
+  'Greq':'>=',
+  'Less':'<',
+  'Leq':'<=' ,
+  'And':'&&' ,
+  'Or':'||',
+  'Negative':'-'
 }
-getCode(s);
-console.log(re)
+s = s.replace(/"/g,'\\"')
+s = s.replace(/'/g,"\"")
+
+
+
+let code = JSON.parse(s);
+var res = "";
+var arr_pp = [];
+function getCode(json_arr) {
+  
+  for(c in json_arr) {
+      if (json_arr[c] instanceof Object) {
+
+          getCode(json_arr[c]);
+          continue;
+      }
+     
+     var s = keyWord(json_arr[c]);
+     if(s!=''){
+        arr_pp.push(s)
+     }
+     
+
+  }
+
+}
+
+function keyWord(c){
+  if(c.search(/KW_Fi/i) == 0){
+    return 'fi';
+  }
+  else if(c.search(/KW_If/i) == 0){
+    return 'if';
+  }
+  else if(c.search(/KW_Do/i) == 0){
+    return 'do';
+  }
+  else if(c.search(/KW_Od/i) == 0){
+    return 'od';
+  }
+  else if(c.search(/KW_Then/i) == 0){
+    return 'then';
+  }
+  else if(c.search(/KW_Else/i) == 0){
+    return 'else';
+  }
+  else if(c.search(/KW_While/i) == 0){
+    return 'while';
+  }
+  else if(c.search(/KW_Read/i) == 0){
+    return 'read';
+  }
+  else if(c.search(/KW_Write/i) == 0){
+    return 'write';
+  }
+  else if(c.search(/KW_Skip/i) == 0){
+    return 'skip';
+  }
+  else if(c.search(/Colon/i) == 0){
+    return ';';
+  }
+  else if(c.search(/Assignment/i) == 0){
+    return ':=';
+  }
+  else if(c.search(/Var/i) == 0 || c.search(/Num/i) == 0){
+    var von = c.split("(")[1].split(",")[0]
+    von = von.replace(/"/g,'')
+    return von;
+  }
+  else if(c.search(/Bracket/i) == 0){
+    
+    var n = c.split('"');
+    
+    return n[1];
+  }
+  else if(c.search(/Op/i) == 0){
+    var von = c.split("(")[1].split(",")[0]
+    von = von.replace(/"/g,'')
+    return OPERATORMAP1[von];
+  }
+  else{
+    return '';
+  }
+  
+}
+
+function Ntab(c){
+  var s = "";
+  for(var i = 0;i<c;i++){
+    s += " ";
+  }
+  return s;
+}
+var one_check = false;
+function Pretty_Printer(arr){
+  var l = arr.length;
+  var level = 0;
+  for (var i = 0;i < l;i++){
+    
+    if(arr[i] == "then" || arr[i] == "do"){
+      res += arr[i] + " ";  
+      level++;
+      res += "\n" + Ntab(level*2);
+    }
+    else if(arr[i] == "fi" || arr[i] == "od"  || arr[i] == "else"){
+      res += "\n";
+      
+      level--;
+      res +=  Ntab(level*2) + arr[i] + " ";
+      if(arr[i] == "else"){
+        
+        level ++;
+        res += "\n" + Ntab(level*2);
+      }
+    }
+    else if(arr[i] == "if"){
+      res +=  arr[i] + " ";
+    }
+    else if(arr[i] == ";"){
+      res += arr[i] + "\n" + Ntab(level*2);
+
+    }
+    else if(arr[i] == "("){
+      res += arr[i]
+
+    }else{
+
+      
+      if(arr[i+1] == ";"){
+        res += arr[i];
+      } 
+      else if(arr[i+1] == ")"){
+        res += arr[i];
+      } else{
+        res +=  arr[i] + " ";
+      }       
+    } 
+  }
+}
+getCode(code);
+Pretty_Printer(arr_pp);
+console.log("Pretty Printer:\n");
+console.log(res);
